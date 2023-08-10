3109ce1bdc33c0643ef6b5c14f1355f6449c158c
raijin0x <139423310+raijin0x@users.noreply.github.com>
2023-08-10 12:52:02 +0700

    test1

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+  },
+  networks: {
+    // for mainnet
+    "base-mainnet": {
+      url: "https://developer-access-mainnet.base.org",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    "base-goerli": {
+      url: "https://goerli.base.org",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    "base-local": {
+      url: "http://localhost:8545",
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      "base-goerli": "DT2W261Q8PFR3GT4JN6CVTRSYRKSS52YIA",
+    },
+    customChains: [
+      {
+        network: "base-goerli",
+        chainId: 84531,
+        urls: {
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org",
+        },
+      },
+    ],
+  },
+
+  defaultNetwork: "hardhat",
+};
+
+export default config;
